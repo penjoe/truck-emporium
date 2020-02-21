19ca50eb26e711c5c114e6b32ab35794481ef224
‘penjoe’ <pennockjoe@gmail.com>
2020-02-21 10:31:03 -0800

    tweaked with the third function script to try and prevent incorrect/unwanted answers

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ function survey() {
     } else if (transmissionType === "automatic") {
         response = "Your kind ain't welcome here";
     }
-
-    return response
+    return response;
 }
 
 
@@ -33,19 +32,18 @@ function purchase() {
   var amount;
   var tinyTruck = '';
 
-  if (truckCount > 10) {
+  if (truckCount >= 10) {
       amount = 'Unfortunately we don\'t have that many trucks in stock!';
   } else if (truckCount >= 5) {
       amount = 'You qualify for a bulk purchase discount!';
-  } else if (truckCount < 5) {
+  } else if (truckCount > 0) {
       amount = 'Thank for for your business!';
-  } else if (isNaN(truckCount)) {
-      amount = 'Please enter a valid amount!';
+  } else if (truckCount === 0 || isNaN(truckCount)) {
+      amount = prompt('Please enter a valid amount!');
   }
 
   for (var i = 1; i <= truckCount; i = i + 1) {
       tinyTruck = tinyTruck + '<p>' + 'Item #' + i + ' ' + '&#128666;' + '</p>';
   }
-
-  return tinyTruck
+  return amount + tinyTruck;
 }
